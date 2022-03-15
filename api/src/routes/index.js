d13377bd054cb51a665941c4ea44e05739c8e5a4
unknown <euge.nannini@gmail.com>
2022-03-15 18:58:56 -0300

    fin

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,6 @@ const axios = require ('axios');
 const {Country, Activity} = require('../db');
 const { response } = require('express');
 const { Op } = require ('sequelize');
-const { reset } = require('nodemon');
 
 
 // Importar todos los routers;
@@ -17,6 +16,7 @@ const router = Router();
 
 
 router.get('/countries', async (req, res)=>{
+
     const name = req.query.name;
     try{
     if(!name){
@@ -51,107 +51,105 @@ router.get('/countries', async (req, res)=>{
     router.get('/countries/:id', async (req,res) => {
     const { id } = req.params;
     const getId = await Country.findByPk(id.toUpperCase(), 
-        {include : Activity}
+        {include : {model : Activity}}
         
     )
     console.log(getId)
     getId ? res.send(getId) : res.send("id not found")
 
     })
+
+
     
-         
-   
-     
-  
-
-  
-
-
-
-
-
-
+ 
 //POST
-router.post('/activity', async (req, res)=> {
-    
-   const {name, difficulty , duration, season, countries} = req.body; //todo lo que necesito de una act
-   Activity.create({
-       name: name,
-       difficulty: difficulty,
-       duration: duration,
-       season: season,
-   })
-   .then((activity) =>{ 
-       countries?.forEach((country) => { //recorro countries y tomo countryid si existe agrego la actividad
-           Country.findByPk(country.id).then((country)=>{
-               if(country) activity.addCountry(country);
-           });
-       });
-   })
-   .catch((error) => {
-       console.log(error);
-   });
-   res.status(200).json({msg:'Activity created!'});
-});
-
 // router.post('/activity', async (req, res)=> {
     
-//     const {name, difficulty , duration, season, countries} = req.body; //todo lo que necesito de una act
-//     const activities = await Activity.create({
-//         name: name,
-//         difficulty: difficulty,
-//         duration: duration,
-//         season: season,
-        
-//     })
-//     console.log(activities)
-//     try{
-//         countries ?.map(async country =>{
-//             let search = await Country.findAll({
-//                 where: {id: country}
-//             })
-//             if(search){ //si hay paises agregalo a las act
-//                 activities.addCountry(country)
-//             }
-//         })
-//         res.send("Activity created")
-//     }catch(e){
-//         res.status(400).send("Sorry, there was an error in creating the activity" + e)
-//     }
-// })
-// router.post('/activity', async (req,res)=>{
-//     const {name, difficulty, duration, season, country} = req.body;
-    
-//     if(!name || !difficulty){
-//         console.log(`Name: ${name}, Difficulty: ${difficulty}`);
-//         return res.status(400).send('Name and difficulty required');
-//     }
-
-//     //saving activities in DB
-//     let actCreated = await Activity.create({
-//         name, difficulty, duration, season
-//     })
-
-//     //Aca establezco la relacion de la act con pais---
-//     let dbCountry = await Country.findAll({
-//         where: { name: country },
-//     })
-//     actCreated.addCountry(dbCountry);
-//     ////////---------------------------
-
-//     res.status(200).send('Activity succesfully created');
-
-    
-    
+//    const {name, difficulty , duration, season, countries} = req.body; //todo lo que necesito de una act
+//    Activity.create({
+//        name: name,
+//        difficulty: difficulty,
+//        duration: duration,
+//        season: season,
+//    })
+//    .then((activity) =>{ //traigo act pasada por body
+//        countries?.forEach((countries) => { //recorro countries y  me fijo si el country pasado en la creacion
+//         //esta y tomo countryid si existe agrego la actividad
+//            Country.findByPk(countries.id).then((countries)=>{ //busca en el modelo country por id y si lo encuentra lo agrega
+//                if(countries) activity.addCountries(countries);
+//            });
+//        });
+//    })
+//    .catch((error) => {
+//        console.log(error);
+//    });
+//    res.status(200).json({msg:'Activity created!'});
 // });
+//        ESTA ANDA OK --------------
+// router.post('/activity', async (req,res)=>{
+//     let {name, difficulty , duration, season, countries} = req.body;
+//     const createActivity = await Activity.create({
+//         name,
+//         difficulty,
+//         duration,
+//         season,
+//     })
+//     if(countries){
+//         await createActivity.addCountries(countries);
+//     }
+//     return res.status(200).json({msg:'Activity created!'})
+// })
+router.post("/activity", async (req,res) => {
+    try {
+        const { name, difficulty, duration, season, countries } = req.body
+        const act = await Activity.create({ name, difficulty, duration, season, })
+        countries.map((e) => act.addCountry(e))
+        res.send(act)
+    } catch (err) {
+        res.send(err)
+    }
+
+    // try{
+    // const {name, difficulty, season, duration, countries} = req.body;
+    // console.log('estos son los countries', countries)
+    // let newTourActivity = await Activity.create({name, difficulty, duration, season, })
+
+    // countries.map((el) => {
+    // return newTourActivity.addCountry(el)
+        
+    // })
+    
+    // res.status(200).send('activity created') 
+    // }catch(error){
+    //     res.send(error)
+    // }
+})  
 
 
+
+// for(let elements of array de nombres){
+//     findOne({aqui el where para buscar})
+//     actividades.addpaises(elemtn)
+// }
+
+
+// router.get('/activity',  async (req,res)=>{
+//     return await Activity.findAll({
+// 		include: {
+// 			model: Country,
+// 			attribute: ['name:', 'flag', 'continents', 'capital'],
+// 			through: {
+// 				attributes: [],
+// 			},
+// 		},
+// 	});
+// })
 
 
 router.get('/activity',  async (req,res)=>{
       
     try{
-           const activities= await Activity.findAll();
+           const activities = await Activity.findAll({})
             return res.status(200).send(activities)
          }catch(error){
              return res.status(400).send(error);
